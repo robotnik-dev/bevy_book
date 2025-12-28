24fe184f36c5fa09e21084e76582d378ca697c2f
robotnik-dev <robotnik.games@gmail.com>
2025-12-28 14:10:25 +0100

    Update apply script to use solution number

--- solutions/apply.sh
+++ solutions/apply.sh
@@ -2,6 +2,19 @@ set -e
 
 folder=$1
 
+if [ -z "$folder" ]; then
+    echo "No solution number specified"
+    exit -1
+fi
+
+if [ -d "solutions/$folder" ]; then
+    # all good
+    echo ""
+else
+    echo "solution/$folder is no directory, please double check solution number"
+    exit -1
+fi
+
 if [[ `git status --porcelain` ]]; then
   echo "Git changes detected, please stash them first:"
   echo "git stash --include-untracked"
