e1b6bdf76e861616332f6bb205c53a3c1252e6e7
robotnik-dev <robotnik.games@gmail.com>
2025-12-28 13:58:14 +0100

    Restructure solutions into folder

--- solutions/apply.sh
+++ solutions/apply.sh
@@ -1,14 +1,16 @@
 set -e
 
+folder=$1
+
 if [[ `git status --porcelain` ]]; then
   echo "Git changes detected, please stash them first:"
   echo "git stash --include-untracked"
   exit 1;
 fi
 
-git apply -3 --ignore-space-change --ignore-whitespace --verbose  solutions/*.patch
+git apply -3 --ignore-space-change --ignore-whitespace --verbose  solutions/$folder/*.patch
 
-echo "Solution applied!"
+echo "Solutions applied!"
 echo ""
 echo "Revert changes with:"
 echo "solutions/restore.sh"
