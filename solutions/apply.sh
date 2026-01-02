770a65da47cb72d5450d0f81ad7635c4dabb0b4e
robotnik-dev <robotnik.games@gmail.com>
2026-01-02 18:08:43 +0100

    update patch strategy

--- solutions/apply.sh
+++ solutions/apply.sh
@@ -1,17 +1,9 @@
 set -e
 
-folder=$1
+solution=$1
 
-if [ -z "$folder" ]; then
+if [ -z "$solution" ]; then
     echo "No solution number specified"
-    exit -1
-fi
-
-if [ -d "solutions/$folder" ]; then
-    # all good
-    echo ""
-else
-    echo "solution/$folder is no directory, please double check solution number"
     exit -1
 fi
 
@@ -21,9 +13,41 @@ if [[ `git status --porcelain` ]]; then
   exit 1;
 fi
 
-git apply -3 --ignore-space-change --ignore-whitespace --verbose  solutions/$folder/*.patch
+# create temp folder for solutions
+mkdir -p temp_solutions
 
-echo "Solutions applied!"
+patch_not_found=1
+
+for patch in solutions/*.patch
+do
+    file=${patch#solutions/}
+    number=${file%-*.patch}
+    if [ $number == $solution ]; then
+        echo "found latest patch: $file"
+        cp solutions/$file temp_solutions
+        patch_not_found=0
+        break
+    fi
+    cp solutions/$file temp_solutions
+done
+
+if [ $patch_not_found == 1 ]; then
+    echo "solution with number: '$solution' not found."
+    echo "Please double check correctness"
+    # remove temp folder again
+    rm -rf temp_solutions
+    exit -1
+fi
+
+echo "The following patches will be applied: "
+ls temp_solutions
+
+git apply -3 --ignore-space-change --ignore-whitespace --verbose  temp_solutions/*.patch
+
+# remove temp folder again
+rm -rf temp_solutions
+
+echo "Done!"
 echo ""
 echo "Revert changes with:"
 echo "solutions/restore.sh"
